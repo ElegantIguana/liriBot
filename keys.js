a231118f9546e870cba59c765682931ec24c9c74
Armand Guim <armandguim3@gmail.com>
2018-01-11 02:11:36 -0500

    basic bot structure

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('Liri Bot now online! ^_^ ');
+
+exports.twitterKeys = {
+  consumer_key: 'jMeILpx0OA3v8QFnenQAfXOIR',
+  consumer_secret: 'xq4b9cgYbOX50rS0wbSBpzfPIzj0LuSnW76cju8yQNlRK1tudc',
+  access_token_key: '951340376404713472-v3t3D135ieGn8kypMbcnJU6GQhLsjTL',
+  access_token_secret: 'lXXdI4cM76k8WCDqjwJ3IrfFMNgG2GDBxLimYl1qB9nEA',
+}
